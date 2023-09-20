d56d485f696cb19ac7c146e85ddd2bf84ce19d97
monet4 <78901182+monet4@users.noreply.github.com>
2023-09-20 10:41:24 +0200

    Update server.js

--- server.js
+++ server.js
@@ -23,22 +23,25 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // MySQL Database Connection
-let db;
+let pool;
 
 (async () => {
     try {
-        db = await mysql.createConnection({
-            host: process.env.DB_HOST, 
-            user: process.env.DB_USER,
-            password: process.env.DB_PASSWORD,
-            database: process.env.DB_NAME, 
-            ssl: {
-                ca: decodedCert
-            }
+        pool = mysql.createPool({
+        host: process.env.DB_HOST, 
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME, 
+        waitForConnections: true,
+        connectionLimit: 15,
+        queueLimit: 0,
+        ssl: {
+            ca: decodedCert
+        }
         });
 
         // Verifica la conexión
-        const [results] = await db.query('SELECT 1');
+        const [results] = await pool.query('SELECT 1');
         console.log('Connected to MySQL:', results);
     } catch (err) {
         console.error('Error connecting to MySQL:', err.stack);
@@ -57,10 +60,32 @@ app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
 
+// Graceful Shutdown
+process.on('SIGTERM', async () => {
+    console.log('SIGTERM signal received. Preparing for graceful shutdown...');
+
+    // Stop the server from accepting new requests.
+    server.close(async () => {
+        console.log('HTTP server closed.');
+
+        // Close any other resources, e.g., the database connection.
+        if (pool && pool.end) {
+            try {
+                await pool.end();
+                console.log('Database connection closed.');
+            } catch (error) {
+                console.error('Error closing the database', error.stack);
+            }
+        }
+
+        process.exit(0);
+    });
+});
+
 // Obtener todas las obras
 app.get('/obras', async (req, res) => {
     try {
-        const [results] = await db.query('SELECT * FROM obras');
+        const [results] = await pool.query('SELECT * FROM obras');
         res.json(results);
     } catch (error) {
         res.status(500).json({ error: error.message });
@@ -71,7 +96,7 @@ app.get('/obras', async (req, res) => {
 app.post('/obras', async (req, res) => {
     const { nombre } = req.body;
     try {
-        const [result] = await db.query('INSERT INTO obras (nombre) VALUES (?)', [nombre]);
+        const [result] = await pool.query('INSERT INTO obras (nombre) VALUES (?)', [nombre]);
         
         // Crear un contenedor para la obra en Azure Blob Storage
         const obraID = result.insertId;
@@ -88,7 +113,7 @@ app.post('/obras', async (req, res) => {
 app.delete('/obras/:id', async (req, res) => {
     const idObra = req.params.id;
     try {
-        await db.query('DELETE FROM obras WHERE idObra = ?', [idObra]);
+        await pool.query('DELETE FROM obras WHERE idObra = ?', [idObra]);
         res.json({ success: true });
     } catch (error) {
         res.status(500).json({ error: error.message });
@@ -105,7 +130,7 @@ app.get('/obras/:idObra/proveedores', async (req, res) => {
             WHERE proveedores_obras.obra_id = ?;
         `;
 
-        const [results] = await db.query(query, [idObra]);
+        const [results] = await pool.query(query, [idObra]);
         res.json(results);
     } catch (error) {
         res.status(500).json({ error: error.message });
@@ -115,7 +140,7 @@ app.get('/obras/:idObra/proveedores', async (req, res) => {
 // Obtener todos los proveedores
 app.get('/proveedores', async (req, res) => {
     try {
-        const [results] = await db.query('SELECT * FROM proveedores');
+        const [results] = await pool.query('SELECT * FROM proveedores');
         res.json(results);
     } catch (error) {
         res.status(500).json({ error: error.message });
@@ -126,7 +151,7 @@ app.get('/proveedores', async (req, res) => {
 app.post('/proveedores', async (req, res) => {
     const { proveedor } = req.body;
     try {
-        const [result] = await db.query('INSERT INTO proveedores (proveedor) VALUES (?)', [proveedor]);
+        const [result] = await pool.query('INSERT INTO proveedores (proveedor) VALUES (?)', [proveedor]);
         res.json({ id: result.insertId });
     } catch (error) {
         res.status(500).json({ error: error.message });
@@ -137,7 +162,7 @@ app.post('/proveedores', async (req, res) => {
 app.delete('/proveedores/:id', async (req, res) => {
     const idProveedor = req.params.id;
     try {
-        await db.query('DELETE FROM proveedores WHERE idProveedor = ?', [idProveedor]);
+        await pool.query('DELETE FROM proveedores WHERE idProveedor = ?', [idProveedor]);
         res.json({ success: true });
     } catch (error) {
         res.status(500).json({ error: error.message });
@@ -147,7 +172,7 @@ app.delete('/proveedores/:id', async (req, res) => {
 // Obtener todas las imágenes
 app.get('/imagenes', async (req, res) => {
     try {
-        const [results] = await db.query('SELECT * FROM imagenes');
+        const [results] = await pool.query('SELECT * FROM imagenes');
         res.json(results);
     } catch (error) {
         res.status(500).json({ error: error.message });
@@ -158,7 +183,7 @@ app.get('/imagenes', async (req, res) => {
 app.post('/imagenes', async (req, res) => {
     const { url, proveedorId, obraId } = req.body;
     try {
-        const [result] = await db.query('INSERT INTO imagenes (url, proveedorId, obraId) VALUES (?, ?, ?)', [url, proveedorId, obraId]);
+        const [result] = await pool.query('INSERT INTO imagenes (url, proveedorId, obraId) VALUES (?, ?, ?)', [url, proveedorId, obraId]);
         res.json({ id: result.insertId });
     } catch (error) {
         res.status(500).json({ error: error.message });
@@ -169,7 +194,7 @@ app.post('/imagenes', async (req, res) => {
 app.delete('/imagenes/:id', async (req, res) => {
     const imagenId = req.params.id;
     try {
-        await db.query('DELETE FROM imagenes WHERE imagenId = ?', [imagenId]);
+        await pool.query('DELETE FROM imagenes WHERE imagenId = ?', [imagenId]);
         res.json({ success: true });
     } catch (error) {
         res.status(500).json({ error: error.message });
@@ -179,7 +204,7 @@ app.delete('/imagenes/:id', async (req, res) => {
 // Obtener todos los registros de proveedores_obras
 app.get('/proveedores_obras', async (req, res) => {
     try {
-        const [results] = await db.query('SELECT * FROM proveedores_obras');
+        const [results] = await pool.query('SELECT * FROM proveedores_obras');
         res.json(results);
     } catch (error) {
         res.status(500).json({ error: error.message });
@@ -190,7 +215,7 @@ app.get('/proveedores_obras', async (req, res) => {
 app.post('/proveedores_obras', async (req, res) => {
     const { proveedor_id, obra_id } = req.body;
     try {
-        const [result] = await db.query('INSERT INTO proveedores_obras (proveedor_id, obra_id) VALUES (?, ?)', [proveedor_id, obra_id]);
+        const [result] = await pool.query('INSERT INTO proveedores_obras (proveedor_id, obra_id) VALUES (?, ?)', [proveedor_id, obra_id]);
         res.json({ id: result.insertId });
     } catch (error) {
         res.status(500).json({ error: error.message });
@@ -201,7 +226,7 @@ app.post('/proveedores_obras', async (req, res) => {
 app.delete('/proveedores_obras/:id', async (req, res) => {
     const proveedor_obra_id = req.params.id;
     try {
-        await db.query('DELETE FROM proveedores_obras WHERE proveedor_obra_id = ?', [proveedor_obra_id]);
+        await pool.query('DELETE FROM proveedores_obras WHERE proveedor_obra_id = ?', [proveedor_obra_id]);
         res.json({ success: true });
     } catch (error) {
         res.status(500).json({ error: error.message });
@@ -211,7 +236,7 @@ app.delete('/proveedores_obras/:id', async (req, res) => {
 // Obtener todos los usuarios
 app.get('/usuarios', async (req, res) => {
     try {
-        const [results] = await db.query('SELECT * FROM usuarios');
+        const [results] = await pool.query('SELECT * FROM usuarios');
         res.json(results);
     } catch (error) {
         res.status(500).json({ error: error.message });
@@ -222,7 +247,7 @@ app.get('/usuarios', async (req, res) => {
 app.post('/usuarios', async (req, res) => {
     const { nombre, apellido, usuario, contrasena, mail } = req.body;
     try {
-        const [result] = await db.query('INSERT INTO usuarios (nombre, apellido, usuario, contrasena, mail) VALUES (?, ?, ?, ?, ?)', [nombre, apellido, usuario, contrasena, mail]);
+        const [result] = await pool.query('INSERT INTO usuarios (nombre, apellido, usuario, contrasena, mail) VALUES (?, ?, ?, ?, ?)', [nombre, apellido, usuario, contrasena, mail]);
         res.json({ id: result.insertId });
     } catch (error) {
         res.status(500).json({ error: error.message });
@@ -233,7 +258,7 @@ app.post('/usuarios', async (req, res) => {
 app.delete('/usuarios/:id', async (req, res) => {
     const idUsuario = req.params.id;
     try {
-        await db.query('DELETE FROM usuarios WHERE idUsuario = ?', [idUsuario]);
+        await pool.query('DELETE FROM usuarios WHERE idUsuario = ?', [idUsuario]);
         res.json({ success: true });
     } catch (error) {
         res.status(500).json({ error: error.message });
@@ -258,7 +283,7 @@ app.post('/uploadImage/:obraID/:proveedorID', upload.single('image'), async (req
 
         const blobUrl = blockBlobClient.url;
 
-        const [result] = await db.query("INSERT INTO imagenes (url, proveedorId, obraId) VALUES (?, ?, ?)", [blobUrl, proveedorID, obraID]);
+        const [result] = await pool.query("INSERT INTO imagenes (url, proveedorId, obraId) VALUES (?, ?, ?)", [blobUrl, proveedorID, obraID]);
 
         if (result.affectedRows > 0) {
             res.send("Imagen subida y URL almacenada correctamente");
@@ -278,7 +303,7 @@ app.get('/getImages/:obraID/:proveedorID', async (req, res) => {
         const proveedorID = req.params.proveedorID;
 
         // Consulta a tu base de datos para obtener las URL de las imágenes asociadas con esta obra y proveedor
-        const [images] = await db.query("SELECT url FROM imagenes WHERE obraId = ? AND proveedorId = ?", [obraID, proveedorID]);
+        const [images] = await pool.query("SELECT url FROM imagenes WHERE obraId = ? AND proveedorId = ?", [obraID, proveedorID]);
 
         res.json(images);
     } catch (error) {
